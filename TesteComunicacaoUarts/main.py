a7ee32770ceebcc731977db36c6c741b63070139
RomuloBenica <50086364+RomuloBenica@users.noreply.github.com>
2020-05-22 15:12:18 -0300

    Update main.py

--- TesteComunicacaoUarts/main.py
+++ TesteComunicacaoUarts/main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
+#escrita e leitura da uart2 placa colibri imx7
+
 import sys
 import serial
 import pynmea2
 import time
-import os.path
-import os
 
-GPIO_PATH = "/sys/class/gpio"
+
 serial_port2 = "/dev/ttymxc1"
 #serial_port3 = "/dev/ttymxc2"
 #serial_port4 =  "/dev/ttymxc3"
@@ -26,17 +26,7 @@ def lerUart(uart):
     arrValorLido = [] 
     mensagemRecebida = []
     try:
-        #exportFile = open(GPIO_PATH+'/export', 'w')
-        #unexportFile = open(GPIO_PATH+'/unexport', 'w')
-        direcaoGpio = open(GPIO_PATH+'/gpio139/direction', 'r')
-        direcaoGpio.write('out')
-        direcaoGpio.flush()
-        valorGpio = open(GPIO_PATH+'/gpio139/value', 'w')
-        print(direcaoGpio)
         while True:
-            valorGpio.write('0')
-            valorGpio.flush()
-
             if uart.in_waiting > 0:
                 valorLido = uart.read()
                 arrValorLido.append(valorLido)
@@ -70,6 +60,6 @@ def escreverNaUart(uartEscreve):
 
 if __name__ == '__main__':
     print('Inicio de leitura e escrita das uarts ...')
-    #escreverNaUart(uart2)
-    #time.sleep(5)
+    escreverNaUart(uart2)
+    time.sleep(2)
     lerUart(uart2)
